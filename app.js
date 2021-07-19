e6382aae169747bdd4d0fe774421d4ea6ee5948c
Shaham Noorani <shahamnoorani@gmail.com>
2021-07-18 22:43:02 -0500

    updated app.js and angular.json

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ const router = require('./api/router');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
