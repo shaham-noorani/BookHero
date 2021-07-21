9e26d2252350cb9baf5b64a7e5ef2616923e366a
Shaham Noorani <shaham.noorani@jbhunt.com>
2021-07-21 16:09:09 -0500

    should allow refreshes now

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
 app.use('/api', router);
+app.use('*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
